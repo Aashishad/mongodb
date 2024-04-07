12842e33107e421e9a4cc2619a237f1c208b2121
Aashishad <aashishrestha15@gmail.com>
2024-04-07 16:18:42 +0545

    ch

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const User = require('../models/User');
+const { userLogin, useRegister } = require('../controllers/userController');
+const validator = require('express-joi-validation').createValidator({});
+const Joi = require('joi');
 const router = express.Router();
 
 
+const loginSchema = Joi.object({
+  email: Joi.string().email().required('mail req'),
+  password: Joi.string().min(5).max(20).required()
+});
 
-router.route('/users/login').post();
-router.route('/users/register').post();
+
+const registerSchema = Joi.object({
+  email: Joi.string().email().required('mail is req'),
+  password: Joi.string().min(5).max(20).required(),
+  fullname: Joi.string().required()
+});
+
+router.route('/users/login').post(validator.body(loginSchema),userLogin);
+router.route('/users/register').post(validator.body(registerSchema),useRegister);
 router.route('/users').get();
 router.route('/users/:id').get().patch();
 
